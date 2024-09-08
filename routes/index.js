9016d79e018dcf94988748f7ec12d183ef370148
Soufiane <sfn.elmansoury@gmail.com>
2024-09-08 04:32:56 +0200

    fetching data with second

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,25 @@ const fetch = require('node-fetch');
 
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`)
-        .then(results => results.json())
-        .then(data => res.json({movies: data.results}))          
-        });
+    .then(results => results.json())
+    .then(data => res.json({movies: data.results}))          
+      
+  });
 
-module.exports = router;
+  const url = 'https://api.themoviedb.org/3/movie/upcoming';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.Access_Token_Auth}`
+    }
+  };
+  
+  fetch(url, options)
+  .then(res => res.json())
+  .then(data => res.json({upcomings: data.results}))
+  .catch(err => console.error('error:' + err));
+
+    
+
+module.exports = router
