220c6c540a1f119b51567db9595637b7837fff02
Soufiane <sfn.elmansoury@gmail.com>
2024-09-21 15:03:35 +0200

    backend API call reviewed

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,24 @@ router.get('/movies', (req, res) => {
       
   });
 
-  const url = 'https://api.themoviedb.org/3/movie/upcoming';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.Access_Token_Auth}`
+  router.get('/upcoming', (req,res) => {
+    const url = 'https://api.themoviedb.org/3/movie/upcoming';
+    const options = {
+      method:'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.Access_Token_Auth}`
+      }
     }
-  };
-  
-  fetch(url, options)
-  .then(res => res.json())
-  .then(data => res.json({upcomings: data.results}))
-  .catch(err => console.error('error:' + err));
 
-    
+    fetch(url,options)
+    .then(res => res.json())
+    .then(data => res.json({upcomingMovies: data.results}))
+    .catch(err => {
+      console.error('Failed To Fetch the Upcoming Movies', err)
+      res.status(500).json({error: 'Failed To Fetch The Upcoming Movies'})
+    })
+  
+  })
 
 module.exports = router
