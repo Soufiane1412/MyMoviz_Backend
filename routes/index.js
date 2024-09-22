55d712d360c170dfd10dc42e3e89772b8945a3b3
Soufiane <sfn.elmansoury@gmail.com>
2024-09-22 23:48:16 +0200

    reviewed backend API call with asynchronous function + try&catch

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,40 @@
 var express = require('express');
 var router = express.Router();
-
 const fetch = require('node-fetch');
 
+const fetchFromAPI = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP Error, status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Error whilst fetching the ${url}`, err)
+    throw error;
+  }
+}
 
-router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`)
-    .then(results => results.json())
-    .then(data => res.json({movies: data.results}))          
-      
-  });
+router.get('/movies', async (req,res) => {
+  try {
+    const data = await fetchFromAPI(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`);
+    res.json({movies: data.reuslts});
+  } catch (error) {
+    console.error('Error in /movies route:' ,error)
+    res.status(500).json({ error: 'Failed to fetch movies data'})
+  } 
+});
 
-
-router.get('/upcomingMovies', (req,res) => {
-  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}`)
-  .then(results => results.json())
-  .then(data => res.json({upcomingMovies: data.results}))
+router.get('/upcomingMovies', async (req,res)=> {
+  try {
+    const data = await fetchFromAPI(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}`);
+    res.json({upcomingMovies: data.results})
+  } catch (error) {
+    console.error('Error in /upcomingMovies route:', error);
+    res.status(500).json({error: 'Failed to fetch upcoming movies data'})
+  }
 })
+
 
   
 module.exports = router
